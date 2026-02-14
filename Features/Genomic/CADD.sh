2f2a0ac5820c22c9a57830043b23b43de06e9d9e
Iman Egab <105759304+iegab@users.noreply.github.com>
2026-02-13 19:31:27 -0600

    Update CADD.sh

--- Features/Genomic/CADD.sh
+++ Features/Genomic/CADD.sh
@@ -1,3 +1,25 @@
+#prepare the dataset for annovar
+annovar_input <- data.frame(
+  chr   = variants_min$chr,
+  start = variants_min$pos,
+  end   = variants_min$pos,
+  ref   = variants_min$ref,
+  alt   = variants_min$alt,
+  stringsAsFactors = FALSE
+)
+
+#save
+write.table(
+  annovar_input,
+  file = "topmed_for_cadd.avinput",
+  sep = "\t",
+  quote = FALSE,
+  row.names = FALSE,
+  col.names = FALSE
+)
+
+
+#on terminal
 perl table_annovar.pl \
   topmed_for_cadd.avinput \
   humandb/ \
