e376e015408466ff855e9bdbbbf1d3854208fc0f
Iman Egab <105759304+iegab@users.noreply.github.com>
2026-04-07 10:54:04 -0500

    Prepare VCF data frame for ANNOVAR
    Added preparation for ANNOVAR by creating a VCF data frame from the 'gregor' data frame.

--- Data/GREGoR.R
+++ Data/GREGoR.R
@@ -5,3 +5,15 @@ length(gr_all)
 names(gr_all) <- make.unique(names(gr_all))
 gregor <- as.data.frame(gr_all)
 #6127
+#prepare for ANNOVAR
+vcf_gregor <- data.frame(
+    CHROM  = paste0("chr", gregor$seqnames),
+    POS    = gregor$start,
+    ID     = ".",
+    REF    = as.character(gregor$ref),
+    ALT    = as.character(gregor$alt),
+    QUAL   = ".",
+    FILTER = "PASS",
+    INFO   = ".",
+    stringsAsFactors = FALSE
+)
