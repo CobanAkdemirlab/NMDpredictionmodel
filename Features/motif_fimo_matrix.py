3cf047d710a868a4f847737f7829602dcf6302bd
Iman Egab <105759304+iegab@users.noreply.github.com>
2026-05-09 14:41:25 -0500

    Add MEME installation instructions to motif_fimo_matrix.py
    Added installation instructions for MEME Suite in the script.

--- Features/motif_fimo_matrix.py
+++ Features/motif_fimo_matrix.py
@@ -6,7 +6,28 @@ from multiprocessing import Pool
 
 import numpy as np
 import pandas as pd
+#to install meme.5.5
+1) Type the following commands and then follow the instructions
+   printed by the "configure" command.
 
+    $ tar zxf meme_VERSION.tar.gz
+    $ cd meme_VERSION 
+    $ ./configure --prefix=$HOME/meme --enable-build-libxml2 --enable-build-libxslt
+# Compile (takes 5-10 minutes)
+make
+ 
+# Install
+make install
+ 
+# Now the bin directory should exist
+ls ~/meme/bin/fimo
+ 
+# Add to PATH
+echo 'export PATH=$HOME/meme/bin:$PATH' >> ~/.zshrc
+source ~/.zshrc
+ 
+# Test
+fimo --version
 # CONFIG
 
 WORKDIR = Path("/path/to/location")
