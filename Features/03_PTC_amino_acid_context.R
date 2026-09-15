bcd562de1bff9ba7dc73eeb53a7bd002d382a6b0
Iman Egab <105759304+iegab@users.noreply.github.com>
2026-09-15 10:23:28 -0500

    Remove Glycine categorization from PTC feature extraction
    Removed categorization of amino acids related to Glycine.

--- Features/03_PTC_amino_acid_context.R
+++ Features/03_PTC_amino_acid_context.R
@@ -452,24 +452,3 @@ message(
     "PTC amino-acid feature extraction complete."
 )
 
-
-#categorization
-#PTC (amino acid)
-df$minus1_is_G <- rep('Not Glycine',nrow(df))
-ind.1 <- which(df$PTC.minus1.aa=='G')
-df$minus1_is_G[ind.1] <- rep('Glycine',length(ind.1))
-
-
-df$minus2_is_G <- rep('Not Glycine',nrow(df))
-ind.1 <- which(df$PTC.minus2.aa=='G')
-df$minus2_is_G[ind.1] <- rep('Glycine',length(ind.1))
-
-df$plus1_is_G <- rep('Not Glycine',nrow(df))
-ind.1 <- which(df$PTC.plus1.aa=='G')
-df$plus1_is_G[ind.1] <- rep('Glycine',length(ind.1))
-
-df$plus2_is_G <- rep('Not Glycine',nrow(df))
-ind.1 <- which(df$PTC.plus2.aa=='G')
-df$plus2_is_G[ind.1] <- rep('Glycine',length(ind.1))
-
-
